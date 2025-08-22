b9c3c30c5e172450a51b1ec63a30110b0ff23600
TrongPhuc209 <nguyentrongphuc1125@gmail.com>
2025-08-22 09:59:30 +0700

    add project

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,46 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.*;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
+import com.example.demo.repository.UserRepository;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    UserRepository userRepository;
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @GetMapping("/gets")
+    public List<User> prinUser() {
+        List<User> users = userRepository.findAll();
+
+        System.out.println("In ra danh sach user: ");
+        for (User i : users) {
+            System.out.println("ID: " + i.getId() + ", name: " + i.getName() + ", age: " + i.getAge());
+        }
+
+        return users;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        if (optionalUser.isPresent()) {
+            return ResponseEntity.ok(optionalUser.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+}
